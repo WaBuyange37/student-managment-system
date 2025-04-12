9c746075b42d1982398b3c513ceaf89cda81005d
WaBuyange37 <nzelabeatblack37@gmail.com>
2025-04-12 14:17:12 +0200

    Student arrray and addStudent func, student pushed in array

---
+++ data.js
@@ -0,0 +1,12 @@
+//array of student object, info + update student info
+const students = []
+
+const addStudent(name,age, gender,className, hobby){
+  const student = {
+    name: name.trim(),
+    age: parseInt(age),
+    gender: gender.trim().toUpperCase(),
+    hobby: hobby.trim(),
+  }
+  students.push(student);
+}
